682c456404dd5bf6088bb3cb8d80136336177a82
daijulong <daijulong@qq.com>
2021-05-05 03:21:51 +0800

    build in windows

---
+++ build.bat
@@ -0,0 +1,14 @@
+:: Mac
+SET CGO_ENABLED=0
+SET GOOS=darwin
+SET GOARCH=amd64
+go build -o dockser_darwin_amd64 main.go
+
+:: Linux
+SET CGO_ENABLED=0
+SET GOOS=linux
+SET GOARCH=amd64
+go build -o dockser_linux_amd64 main.go
+
+:: Windows
+go build -o dockser_windows_amd64.exe main.go
